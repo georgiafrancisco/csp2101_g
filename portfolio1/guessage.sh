c3e7e8dfb516c1f4f4b76d13e5431c6eaeb8997e
Ubuntu <labuser@ML-RefVm-262326.h54rnubfqvvexo03i3dcrjaq1h.px.internal.cloudapp.net>
2020-04-09 11:55:34 +0000

    This is my portfolio1

---
+++ portfolio1/guessage.sh
@@ -0,0 +1,33 @@
+#Georgia Francisco - 10424443
+
+#! /bin/bash
+
+#Define the variables
+ 
+guess=-1
+typeset -i num=0
+
+#Introduce the game
+
+echo -e "AGE GUESSING GAME\n"
+
+#Generate a random number(age)
+
+((answer = RANDOM % 99))
+
+#Providing a user input and aks user to provide input
+#Giving indication whether the input is too high or low
+
+while ((guess != answer)); do
+      num=num+1
+      read -p "Guess an AGE (HINT: It's below 99 ;) ):" guess
+      if ((guess < answer)); then
+         echo "Higher!"
+      elif ((guess > answer)); then
+         echo "Lower!"
+      fi 
+done
+
+#Print a message to user if they have guessed the age correctly
+
+echo -e "CORRECT! YOU GUESSED THE AGE!!"
